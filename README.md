38461a3973ff7fc08fcd4844d73805312dd37720
jw <choi.jae100@gmail.com>
2025-11-11 15:39:57 -0800

    install fastapi, create outline

---
+++ README.md
@@ -0,0 +1,3 @@
+# Twitch Log Client
+
+View logs using Chatterino
---
+++ tlc/tlc.py
@@ -0,0 +1,37 @@
+
+class Tlc:
+    def __init__(self, host: str):
+        self.host = host
+
+    def parse_log(self, filename: str) -> str:
+        """
+        Args:
+            filename (str): 
+            example (mac m1): 
+                /Users/{UserName}/Library/Application Support/chatterino/Logs/Twitch/Channels/{StreamerName}/{StreamerName}-2025-11-11.log
+        """
+        return 
+
+    def save_log(self) -> str:
+        return
+
+    def _clear_log(self):
+        return
+
+
+    def _handle_startup(self):
+        return
+
+
+#################################################
+
+def process_loop():
+    return
+
+    
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
