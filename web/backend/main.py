da6d4bef1be764de1d59c911d9e97ffd0a8c2e43
jw <choi.jae100@gmail.com>
2025-11-20 14:26:37 -0800

    update chat filtering logic, fix lifespan warning

--- web/backend/main.py
+++ web/backend/main.py
@@ -1,11 +1,13 @@
 from contextlib import asynccontextmanager
-from typing import Annotated
+from typing import Annotated, Literal, Optional
 
+from pydantic import BaseModel
 from starlette.middleware.cors import CORSMiddleware
-import httpx
-from starlette.middleware import Middleware
 
-from fastapi import Depends, FastAPI, HTTPException, Query
+# import httpx
+# from starlette.middleware import Middleware
+
+from fastapi import Depends, FastAPI, HTTPException, Query, Request, Response
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
 import uuid
@@ -35,7 +37,7 @@ class ChatMessage(SQLModel, table=True):
     channel_name: str = Field(index=True)
     username: str | None = Field(default=None, index=True)
     message_text: str
-    message_type: str
+    message_type: str = Field(index=True)
 
 
 ##############################################################################
@@ -59,26 +61,6 @@ def get_session():
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
-##############################################################################
-
-
-def add_cors_middleware(app):
-    return CORSMiddleware(
-        app=app,
-        allow_origins=["*"],
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-
-
-def add_logging_middleware(app):
-    async def middleware(scope, receive, send):
-        path = scope["path"]
-        print("Request:", path)
-        await app(scope, receive, send)
-
-    return middleware
-
 
 ##############################################################################
 
@@ -91,34 +73,63 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-app.add_middleware(add_cors_middleware)
-app.add_middleware(add_logging_middleware)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.middleware("http")
+async def add_logging_middleware(request: Request, call_next):
+    print(f"Request path: {request.url.path}")
+    response: Response = await call_next(request)
+    print(f"Response status: {response.status_code}")
+    return response
 
 
 ##############################################################################
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/chats/")
-def get_chats(
+@app.get("/chats/", response_model=list[ChatMessage])
+async def read_chats(
     session: SessionDep,
-    channel_name: Annotated[str, Query(title="Channel name")] = None,
+    channel_name: Annotated[str | None, Query()] = None,
+    username: Annotated[str | None, Query()] = None,
+    message_type: Annotated[str | None, Query()] = None,
+    start_datetime: Optional[datetime] = Query(None),
+    end_datetime: Optional[datetime] = Query(None),
     offset: int = 0,
-    limit: Annotated[int, Query(le=100)] = 100,
+    limit: Annotated[int, Query(ge=1, le=100)] = 100,
+    order_by: Annotated[
+        Literal["timestamp", "username", "message_type"], Query()
+    ] = "timestamp",
+    desc: bool = False,
 ) -> list[ChatMessage]:
+    stmt = select(ChatMessage)
 
-    if channel_name is None:
-        stmt = select(ChatMessage).offset(offset).limit(limit)
+    if channel_name is not None:
+        stmt = stmt.where(ChatMessage.channel_name == channel_name)
+
+    if username is not None:
+        stmt = stmt.where(ChatMessage.username == username)
+
+    if message_type is not None:
+        stmt = stmt.where(ChatMessage.message_type == message_type)
+
+    if start_datetime is not None:
+        stmt = stmt.where(ChatMessage.timestamp >= start_datetime)
+
+    if end_datetime is not None:
+        stmt = stmt.where(ChatMessage.timestamp <= end_datetime)
+
+    if desc:
+        stmt = stmt.order_by(getattr(ChatMessage, order_by).desc())
     else:
-        stmt = (
-            select(ChatMessage)
-            .where(ChatMessage.channel_name == channel_name)
-            .offset(offset)
-            .limit(limit)
-        )
+        stmt = stmt.order_by(getattr(ChatMessage, order_by))
+
+    stmt = stmt.offset(offset).limit(limit)
+
     results = session.exec(stmt).all()
     return results
